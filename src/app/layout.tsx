bf944233bc5f4720f9762bd721256d394882dd07
EchoWraith <echow@EchoWraith>
2025-08-05 10:41:27 +0100

    Fix large screens problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className=" relative">
-        <div className="max-w-[1920px] ">
+        <div className="">
 
         {/* <Header /> */}
         <Main />
